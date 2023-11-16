820bbe3b0ad336d93b6ada37b66dd864990af33a
Anthony Kumasaka <anthony@shiftsmart.com>
2023-11-16 22:22:03 +0000

    chore: setup basic react app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { LandingPage } from './components/LandingPage';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <LandingPage/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,7 @@
+export const LandingPage = () => {
+    return (
+        <div>
+            <h1>This is the Landing Page</h1>
+        </div>
+    )
+}
